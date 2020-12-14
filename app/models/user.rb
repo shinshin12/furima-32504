353983a2fffb04b84c6bc6151a291783b3d2704b
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2020-12-15 02:50:16 +0900

    新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    
+  validates :nickname, presence: true
+  validates :surname, presence: true
+  validates :name, presence: true
+  validates :kana_surname, presence: true
+  validates :kana_name, presence: true
+  validates :birthday_id, presence: true
 end
