6368bed130c8908f514541c0b206d21305fcf8ba
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2020-12-15 16:04:24 +0900

    バリデーションの正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_PASSWORD_REGEX = /[a-z\d]{6,}/i
+  validastes :password, pressence:true, format: {with: VALID_PASSWORD_REGEX}
     
   validates :nickname, presence: true
-  validates :surname, presence: true
-  validates :name, presence: true
-  validates :kana_surname, presence: true
-  validates :kana_name, presence: true
+  validates :surname, presence: true, format: {with: [^ -~｡-ﾟ]*$/}
+  validates :name, presence: true, format: {with: [^ -~｡-ﾟ]*$/}
+  validates :kana_surname, presence: true, format: {with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/}
+  validates :kana_name, presence: true, format: {with: /[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+/}
   validates :birthday_id, presence: true
 end
