ceb372717b32c927b838287a9e5104562e271baa
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2020-12-17 22:38:00 +0900

    active_strageの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,18 @@
 class Item < ApplicationRecord
-  
+
+ belongs_to :user
+ has_one_attached :image
+
+
+
+ with_options presence: true do
+ item_name		
+ explanation	
+ category_id	
+ state_id	
+ shipping_method_id	
+ profecture_id	
+ date_id	
+ price
+ end
 end
