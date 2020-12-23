c27ae0e45f83d2971f92a2ee96bb53619dc71c22
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2020-12-23 16:06:58 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,8 +29,11 @@ class ItemsController < ApplicationController
    end
   end
   def destroy
-    @item.destroy
+   if  @item.destroy
     redirect_to root_path
+   else
+    render :show
+   end
   end
 
   private
