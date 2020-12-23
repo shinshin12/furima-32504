30950012b0657919d272e89c0bcd50de88cab0df
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2020-12-23 14:15:57 +0900

    Revert "編集画面に遷移の制限"
    This reverts commit c3c0132a4e11f1a0a4634dd7c6e1ca54d29b3707.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, only: [:new]
   def index
   @items = Item.all.order(created_at: :desc)
   end
@@ -35,10 +34,4 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :explanation, :price, :category_id, :state_id, :shipping_method_id, :prefecture_id, :day_time_id, :image).merge(user_id: current_user.id)
   end
-  def move_to_index
-    @item = Item.find(params[:id])
-     unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path
-    end
- end
 end
