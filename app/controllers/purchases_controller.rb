3e091f3052eadb3bab5dde0f20526738fd435a83
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2020-12-29 13:40:43 +0900

    pay.jpの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,27 +1,30 @@
 class PurchasesController < ApplicationController
   def index
-    @purchase = Payment.new
     @item = Item.find(params[:item_id])
+    @payment_form = PaymentForm.new
   end
+
   def create
-    @purchase = Payment.new(purchase_params)
-    if @purchase.valid?
+    @item = Item.find(params[:item_id])
+    @payment_form = PaymentForm.new(purchase_params)
+    if @payment_form.valid?
       pay_item
-      @purchase.save
+      @payment_form.save
      return redirect_to root_path  
     else
     render "index"
+    end
   end
   private
   def purchase_params
-    params.require(:payment).permit(:postal_code, :prefecture_id, :city, :house_number, :builing_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token:params[:token])
+    params.require(:payment_form).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp.api_key = 
-      sk_test_ba755a676720e375eaa7ed0d
+      "sk_test_ba755a676720e375eaa7ed0d"
       Payjp::Charge.create(
-        amount: purchase_params[:price],  
+        amount: @item.price,  
         card: purchase_params[:token],    
         currency: 'jpy'                 
       )
--- app/models/payment_form.rb
+++ app/models/payment_form.rb
@@ -1,19 +1,18 @@
-class Payment
+class PaymentForm
   include ActiveModel::Model
-  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id :token
+  attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
   validates :postal_code
   validates :prefecture_id
   validates :city
   validates :house_number
-  validates :building_name
   validates :phone_number
   validates :token
   end
 
   def save
-  purchase = Purchase.create(user_id: user.id, item_id: item.id)
+  purchase = Purchase.create(user_id: user_id, item_id: item_id)
   Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)  
   end
 end
