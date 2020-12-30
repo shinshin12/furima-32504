fdb861fe684456ac3d74486f5f885871d273806b
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2020-12-30 12:48:32 +0900

    修正１

--- app/models/payment_form.rb
+++ app/models/payment_form.rb
@@ -15,6 +15,6 @@ class PaymentForm
 
   def save
   purchase = Purchase.create(user_id: user_id, item_id: item_id)
-  Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase_id)  
+  Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)  
   end
 end
