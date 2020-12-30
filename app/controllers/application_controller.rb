56d39943ecb32d6d3f29bf36e02ac60c99adfac3
shinshin12 <tomo1997.kyoto1.2@gmail.com>
2020-12-30 14:42:26 +0900

    basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_parameter_devise, if: :devise_controller?
 
   private
    def configure_parameter_devise
      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :surname, :name, :kana_surname, :kana_name, :birthday])
    end
+   def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
